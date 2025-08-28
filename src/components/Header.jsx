be240b434ef8fc58bb1ae0e5653135b9046871dd
Carlos Duarte <carlos.duarte2@mx.zurich.com>
2025-08-28 13:16:59 -0700

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+
+function Header() {
+  return (
+    <Fragment>
+        <p>Desde Header</p>
+        <p>dos dos</p>
+    </Fragment>
+  )
+}
+
+export default Header
